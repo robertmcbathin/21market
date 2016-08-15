55e84730e559adad0aa1bc876dbaa8ca48fab0c0
robertmcbathin <mercile55@yandex.ru>
2016-08-15 07:08:38 +0300

    wtf

---
+++ resources/views/sections/product_thumbnail.blade.php
@@ -0,0 +1,21 @@
+<div class="col-sm-4 col-lg-4 col-md-4">
+<div class="thumbnail">
+     <img src="{{ $product->path_to_img }}" alt="{{ $product->name }}" class="img-responsive" height="150" >
+                        <div class="caption" >
+                        <h4>
+                              <a href="#">{{ $product->name }}</a>
+                          </h4>
+                          <h4 class="pull-right">{{ $product->price }}</h4>
+                          </div>
+                          <div class="ratings">
+                            <p class="pull-right">15 reviews</p>
+                            <p>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -93,27 +93,7 @@
                     <h2>{{ $subcategory->name }}</h2>
                     @foreach ($products as $product)
                     @if ($product->subcategory_id == $subcategory->id)
-                      <div class="col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                          <img src="{{ $product->path_to_img }}" alt="{{ $product->name }}" height="150" >
-                        <div class="caption" >
-                        <h4>
-                              <a href="#">{{ $product->name }}</a>
-                          </h4>
-                          <h4 class="pull-right">{{ $product->price }}</h4>
-                          </div>
-                          <div class="ratings">
-                            <p class="pull-right">15 reviews</p>
-                            <p>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
+                        @include('sections.product_thumbnail')
                     @endif
                     @endforeach
                   </div>
